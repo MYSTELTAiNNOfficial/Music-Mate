277222a7b7309deb11d22e8158ac4cbc5067dbe2
Nathanael Abel <yujioshinozaki@gmail.com>
2023-05-28 22:13:17 +0700

    Firebase Login Feature

--- lib/views/pages/pages.dart
+++ lib/views/pages/pages.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:just_audio/just_audio.dart';
